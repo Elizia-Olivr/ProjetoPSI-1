324424e5cd5563624ca8911e07d07792cd878c19
Elízia Regina de Oliveira Silva <elizia.regina@academico.ifrn.edu.br>
2022-08-11 11:31:58 -0300

    Alteração-index-produto

--- WebApplication1/Models/Produto.cs
+++ WebApplication1/Models/Produto.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Web;
 using WebApplication1.Models;
 
-public class Produto
+namespace WebApplication1.Models
 {
-    public long? ProdutoId { get; set; }
-    public string Nome { get; set; }
-    public long? CategoriaId { get; set; }
-    public long? FabricanteId { get; set; }
-    public Categoria Categoria { get; set; }
-    public Fabricante Fabricante { get; set; }
+    public class Produto
+    {
+        public long? ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public long? CategoriaId { get; set; }
+        public long? FabricanteId { get; set; }
+        public Categoria Categoria { get; set; }
+        public Fabricante Fabricante { get; set; }
+    }
 }
